ab597704a096572301a0d24fdc5c8fee3eaf2810
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2024-03-15 01:46:22 +0800

    Update Dockerfile and compose.yaml, add frontend and backend services (#4)

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,4 +1,8 @@
+import os
+
 from flask import Flask
+
+BACKEND_CONTAINER_PORT = os.getenv("BACKEND_CONTAINER_PORT", "5000")
 
 app = Flask(__name__)
 
@@ -7,4 +11,4 @@ def dummy_route():
     return 'Hello, this is the dummy route!'
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0", port=5000)
+    app.run(debug=True, host="0.0.0.0", port=BACKEND_CONTAINER_PORT)
