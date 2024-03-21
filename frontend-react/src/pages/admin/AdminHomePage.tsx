a19cf96c48ef7ffc86258771e46df77f8a1e9990
Nixon <nixonwidjaja23@gmail.com>
2024-03-22 00:39:08 +0800

    admin home

--- frontend-react/src/pages/admin/AdminHomePage.tsx
+++ frontend-react/src/pages/admin/AdminHomePage.tsx
@@ -1,36 +1,32 @@
-import { Flex, Grid, GridItem } from "@chakra-ui/react"
+import { Flex, Grid, GridItem, Heading, VStack } from "@chakra-ui/react"
 import SurveyCard from "./SurveyCard"
 import NewSurveyCard from "./NewSurveyCard"
 
 function AdminHomePage() {
   return (
     <Flex minH="100vh" w="100%" bg="gray.100" minW="80rem">
-      <Grid
-        templateColumns="repeat(3, 1fr)"
-        gap={12}
-        mx="auto"
-        my="10rem"
-        h="100%"
-        w="70rem"
-      >
-        <GridItem w="100%" h="20rem">
-          <NewSurveyCard />
-        </GridItem>
-        <GridItem w="100%" h="20rem">
-          <SurveyCard />
-        </GridItem>
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-        <GridItem w="100%" h="20rem" />
-      </Grid>
+      <VStack mx="auto" my="5rem" spacing="3rem">
+        <Heading mx="auto" as="h1" fontSize="5xl">
+          Recent Surveys
+        </Heading>
+        <Grid templateColumns="repeat(3, 1fr)" gap={12} h="100%" w="70rem">
+          <GridItem w="100%" h="20rem">
+            <NewSurveyCard />
+          </GridItem>
+          <GridItem w="100%" h="20rem">
+            <SurveyCard />
+          </GridItem>
+          <GridItem w="100%" h="20rem">
+            <SurveyCard />
+          </GridItem>
+          <GridItem w="100%" h="20rem">
+            <SurveyCard />
+          </GridItem>
+          <GridItem w="100%" h="20rem">
+            <SurveyCard />
+          </GridItem>
+        </Grid>
+      </VStack>
     </Flex>
   )
 }
