ba0e81cc19c38906536fe74f67ce83a6367a252f
Nixon Widjaja <69686938+nixonwidjaja@users.noreply.github.com>
2024-03-22 19:44:08 +0800

    create survey structure (#27)

---
+++ frontend-react/src/pages/admin/survey/AdminSurveyAccordion.tsx
@@ -0,0 +1,63 @@
+import { DeleteIcon } from "@chakra-ui/icons"
+import {
+  AccordionItem,
+  AccordionButton,
+  AccordionPanel,
+  AccordionIcon,
+  Flex,
+  Input,
+  Select,
+  Button,
+  VStack,
+} from "@chakra-ui/react"
+
+function AdminSurveyAccordion() {
+  return (
+    <AccordionItem
+      w="48rem"
+      bg="white"
+      borderTop={0}
+      borderRadius={5}
+      mt="1rem"
+    >
+      <AccordionButton h="5rem" borderBottom="1px" borderColor="gray.200">
+        <Flex
+          alignItems="center"
+          justifyContent="space-between"
+          w="full"
+          p="0.5rem"
+        >
+          <Input
+            placeholder="Question"
+            variant="unstyled"
+            size="lg"
+            fontSize="2xl"
+            fontWeight="bold"
+            w="42rem"
+            onClick={(e) => e.stopPropagation()}
+            onKeyUp={(e) => e.preventDefault()}
+          />
+          <AccordionIcon />
+        </Flex>
+      </AccordionButton>
+      <AccordionPanel p="1.5rem">
+        <VStack spacing="2rem">
+          <Select
+            defaultValue={"option1"}
+            justifyContent="left"
+            alignContent="start"
+          >
+            <option value="option1">Multiple Choice Question</option>
+            <option value="option2">Multiple Response Question</option>
+            <option value="option3">Short Answer</option>
+          </Select>
+          <Button leftIcon={<DeleteIcon />} colorScheme="red" variant="outline">
+            Delete
+          </Button>
+        </VStack>
+      </AccordionPanel>
+    </AccordionItem>
+  )
+}
+
+export default AdminSurveyAccordion
