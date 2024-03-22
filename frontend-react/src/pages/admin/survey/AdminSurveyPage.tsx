c4a3b30a96a38ce2024cc77563edf946c793f70c
Nixon <nixonwidjaja23@gmail.com>
2024-03-22 19:42:55 +0800

    create survey structure

--- frontend-react/src/pages/admin/survey/AdminSurveyPage.tsx
+++ frontend-react/src/pages/admin/survey/AdminSurveyPage.tsx
@@ -1,5 +1,51 @@
+import {
+  Accordion,
+  Button,
+  Card,
+  Flex,
+  HStack,
+  Input,
+  VStack,
+} from "@chakra-ui/react"
+import AdminSurveyAccordion from "./AdminSurveyAccordion"
+import { AddIcon, ExternalLinkIcon } from "@chakra-ui/icons"
+
 function AdminSurveyPage() {
-  return <div>Admin Survey Page</div>;
+  return (
+    <Flex minH="100vh" w="100%" bg="gray.100" minW="80rem">
+      <VStack mx="auto" my="5rem" spacing="0" w="48rem">
+        <Card w="48rem" bg="white" p="1.5rem">
+          <Input
+            placeholder="Untitled"
+            variant="flushed"
+            size="lg"
+            fontSize="4xl"
+            fontWeight="bold"
+            autoFocus
+          />
+          <Input
+            placeholder="Description"
+            variant="flushed"
+            size="md"
+            fontSize="xl"
+            mt="1rem"
+          />
+        </Card>
+        <Accordion allowMultiple allowToggle defaultIndex={[0, 1]}>
+          <AdminSurveyAccordion />
+          <AdminSurveyAccordion />
+        </Accordion>
+        <HStack mt="1rem" w="full">
+          <Button leftIcon={<AddIcon />} colorScheme="green">
+            Add Question
+          </Button>
+          <Button leftIcon={<ExternalLinkIcon />} colorScheme="messenger">
+            Create Survey
+          </Button>
+        </HStack>
+      </VStack>
+    </Flex>
+  )
 }
 
-export default AdminSurveyPage;
+export default AdminSurveyPage
