cdb18335829fd9a62e2868a6541c501e4c1c26a7
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-23 16:34:36 +0800

    add docs

--- backend/src/llm_classes/pipeline_level.py
+++ backend/src/llm_classes/pipeline_level.py
@@ -2,14 +2,21 @@ from llm_level import *
 
 
 class Pipeline:
-    SYSPROMPT = "INSERT SYSTEM PROMPT"
-    START = {
-        "role": "system",
-        "content": SYSPROMPT
-    }
+    # Sysprompt To be updated
+    SYSPROMPT = """You are an assistant who is trying to gather user responses for a product.
+        You have collected some survey responses, and you would like to probe further about what the user thinks about the product.
+        The user responses are provided below. Given these responses, pretend you are an interview and please generate one and only one question for the user.
+        {survey_initial_responses}""" 
+    
 
-    def __init__(self, message_list: list, start_items=1):
-        self.message_list = [Pipeline.START]
+    def __init__(self, survey_initial_responses: str, start_items=1):
+        
+        start_dict = {
+            "role": "system",
+            "content": Pipeline.SYSPROMPT.format(survey_initial_responses = survey_initial_responses)
+        }
+
+        self.message_list = [start_dict]
         self.current_index = 1
         
 
@@ -31,10 +38,18 @@ class Pipeline:
         )
         self.current_index = index + 1 
         self.message_list = self.message_list[:self.current_index]
-        return message
+        return self.message_list
     
 
 
 
-
-        
+################ TEST HERE ######################
+# string = """
+# 1. What do you like about colgate toothpaste?
+# Tastes okay, makes teeth feel good
+# 2. Would you recommend it to a friend?
+# No
+# """
+# llm = GPT()
+# pipe = Pipeline(string)
+# print(llm.run(pipe.message_list))
