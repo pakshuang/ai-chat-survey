03f6fa6e852383c877b840a59d3a0c41a4110abb
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-24 04:25:02 +0800

    add docs and create basic files

--- backend/src/llm_classes/llm_level.py
+++ backend/src/llm_classes/llm_level.py
@@ -5,12 +5,19 @@ from openai import OpenAI
 
 
 class LLM(ABC):
+    '''
+    A large language model class. 
+    Supports extension to other APIs/other LLM types such as local open-source LLMs.
+    '''
     @abstractmethod
     def run(self, messages: list) -> str:
         pass
 
 
 class GPT(LLM):
+    '''
+    Wrapper class around GPT-4 from OpenAI.
+    '''
     def __init__(self, model="gpt-4"):
         load_dotenv()
         client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
---
+++ backend/src/survey_creation/survey_classes.py
@@ -0,0 +1,18 @@
+class Survey:
+    '''
+    Define survey
+    '''
+    def __init__(self):
+        '''
+        Fill in details.
+        '''
+        pass
+
+class Question:
+    '''
+    A survey question. When we feed into GPT, need to format them right.
+    I suggest each type of question aka mrq, mcq be its own subtype, and we can format them differently.
+    '''
+
+    def __init__(self):
+        pass
