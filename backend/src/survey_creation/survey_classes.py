8e4444495920bc8943cc14b2c074d83e5b05012e
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-24 05:32:26 +0800

    modify survey classes

--- backend/src/survey_creation/survey_classes.py
+++ backend/src/survey_creation/survey_classes.py
@@ -8,33 +8,72 @@ class SurveyLoadException(Exception):
         self.message = message
         super().__init__(self.message)
 
-class Survey:
-    '''
-    Define survey for ORM use
-    '''
-    def __init__(self, json_string: str):
-        try:
-            to_json = json.loads(json_string)
-            self.metadata = to_json['metadata']
-            self.title = to_json['title']
-            self.subtitle = to_json['subtitle']
-            self.questions = self.format_questions(to_json['questions'])
-            self.survey_chat_context = to_json['survey_chat_context']
-
-        except Exception as e:
-            raise SurveyLoadException(e) from None
-    
-    def format_questions(self, questions: list):
-        return list(
-            map(lambda question: Question(question), questions)
-        )
-
 class Question:
     '''
     A survey question. When we feed into GPT, need to format them right.
     I suggest each type of question aka mrq, mcq be its own subtype, and we can format them differently.
     '''
 
-    def __init__(self, data: dict):
-        self.id = data['id']
-        self.type = data['type']
+    def __init__(self, id: str, type: str):
+        self.id = id
+        self.type = type
+        self.data = {
+            "id": self.id,
+            "type": self.type
+            # TO BE UPDATED
+        }
+
+    def as_dict(self) -> dict[str, object]:
+        return self.data
+    
+class Survey:
+    '''
+    Constructs a survey. Probably client will use this when building survey.
+    '''
+    def __init__(
+            self, metadata: str, 
+            title: str, 
+            subtitle: str, 
+            questions: list[Question], 
+            survey_chat_context: str):
+        
+        self.metadata = metadata
+        self.title = title
+        self.subtitle = subtitle
+        self.questions = questions
+        self.survey_chat_context = survey_chat_context
+
+    def as_dict(self) -> dict[str, object]:
+        
+        return {
+            "metadata": self.metadata,
+            "title": self.title,
+            "subtitle": self.subtitle,
+            "questions": list(map(lambda question: question.as_dict(), self.questions)),
+            "survey_chat_context": self.survey_chat_context
+        }
+
+
+def create_questions_from_list(questions: list[dict]) -> list[Question]:
+        return list(
+            map(lambda question: Question(question["id"], question["type"]), questions)
+        )
+
+def construct_survey_from_json(json_string: str) -> Survey:
+    '''
+    Accepts a json string in the format of a survey and returns a survey object.
+    '''
+    try:
+        to_json = json.loads(json_string)
+
+        survey = Survey(
+            to_json['metadata'],
+            to_json['title'],
+            to_json['subtitle'],
+            create_questions_from_list(to_json['questions']),
+            to_json['survey_chat_context']
+        )
+        return survey
+        
+    except Exception as e:
+        raise SurveyLoadException(e) from None
