9a1e051826640343e5523cbc4f71d19f14f0195f
CH <80204438+limchoonhao@users.noreply.github.com>
2024-03-23 09:52:17 +0000

    Edited yaml and reformatted py files to comply with linter

--- backend/src/llm_classes/llm_level.py
+++ backend/src/llm_classes/llm_level.py
@@ -3,19 +3,21 @@ from abc import ABC, abstractmethod
 from dotenv import load_dotenv
 from openai import OpenAI
 
+
 class LLM(ABC):
     @abstractmethod
     def run(self, messages: list) -> str:
         pass
 
+
 class GPT(LLM):
     def __init__(self, model="gpt-4"):
         load_dotenv()
-        client = OpenAI(api_key=os.getenv('OPENAI_API_KEY'))
+        client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
         self.client = client
         self.model = model
         super().__init__()
-        
+
     def run(self, messages: list) -> str:
         output = self.client.chat.completions.create(
             model=self.model,
@@ -23,4 +25,3 @@ class GPT(LLM):
             stream=False,
         )
         return output.choices[0].message.content
-
