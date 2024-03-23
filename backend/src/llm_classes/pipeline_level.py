bb880f1ccd3d9216d33de0bb6f371a54190cf971
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-23 16:12:03 +0800

    remove stage abstraction

--- backend/src/llm_classes/pipeline_level.py
+++ backend/src/llm_classes/pipeline_level.py
@@ -1,47 +1,38 @@
 from llm_level import *
 
-class Stage(ABC):
-    def __init__(self):
-        self.next = None
-        self.input = None
-
-    @abstractmethod
-    def forward(self, inputs):
-        pass
-
-    @abstractmethod
-    def process_inputs(self, inputs):
-        pass
-
-class LLMStage(Stage):
-    def __init__(self, llm: LLM, process_function):
-        self.llm = llm
-        self.process_function = process_function # function that processes text by doing prompt engineering
-        super().__init__()
-    
-    def forward(self, inputs: str) -> str:
-        return self.llm.run(self.process_inputs(inputs))
-
-    def process_inputs(self, inputs: str) -> list:
-        '''
-        Converts string into json array format for feeding into llm. Also processes text aka perform prompt engineering
-        '''
-        out = self.process_function(inputs)
-        # Still needs stuff
-        return out # messages
 
 class Pipeline:
-    def __init__(self, *list_of_stages, start_index=0):
-        self.list_of_stages = list_of_stages
-        self.current_index = start_index
+    SYSPROMPT = "INSERT SYSTEM PROMPT"
+    START = {
+        "role": "system",
+        "content": SYSPROMPT
+    }
 
-    def run(self, initial_input: list):
+    def __init__(self, message_list: list, start_items=1):
+        self.message_list = [Pipeline.START]
+        self.current_index = 1
+        
+
+    def insert_and_update(self, message: str, index: int, is_llm: bool):
         '''
-        Gets output from the stage, decide whether to continue conversation chain, add new stage and update pointer,
-        or if the user is editing a reply, set pointer to the stage the index has updated and delete all after.
+        Add a new reply to the conversation chain. If edits are made in the middle, future conversations are deleted.
         '''
-        # use self.list_of_stages[current_index].forward()
-        return
+        if is_llm:
+            role = "assistant"
+        else:
+            role = "user"
+
+        self.message_list.insert(
+            index,
+            {
+                "role": role,
+                "content": message
+            }
+        )
+        self.current_index = index + 1 
+        self.message_list = self.message_list[:self.current_index]
+        return message
+    
 
 
 
