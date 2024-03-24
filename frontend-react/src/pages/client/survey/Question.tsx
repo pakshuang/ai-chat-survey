07596bc7cfaadd6922994fa60d13c87315bc4f94
msh <83120007+johntanas@users.noreply.github.com>
2024-03-24 16:46:38 +0800

    feat: survey questions

--- frontend-react/src/pages/client/survey/Question.tsx
+++ frontend-react/src/pages/client/survey/Question.tsx
@@ -1,26 +1,80 @@
-import React from "react";
-import { Box, FormControl, FormLabel,Flex,IconButton} from '@chakra-ui/react';
-import { StarIcon } from '@chakra-ui/icons';
-interface RatingQuestionProps {
-    questionData: any; // Adjust the type according to your data structure
-    handleQuestionResponse: (id: any, key: any, val: any) => void; // Adjust the parameter types according to your requirements
-  }
-  
-const RatingQuestion = ({questionData,handleQuestionResponse}:RatingQuestionProps)=>{
-    return <FormControl id="email">
-    <FormLabel fontSize="xl"></FormLabel>
-    <Flex>
-      {[1, 2, 3, 4, 5].map((star) => (
-        <Box key={star} as="span" mr="1">
-          <IconButton
-            aria-label={`Star ${star}`}
-            icon={<StarIcon color={star <= (questionData.rating || 0) ? 'teal.500' : 'gray.200'} />}
-            onClick={() => handleQuestionResponse(questionData.id,'rating',star)}
-          />
-        </Box>
-      ))}
-    </Flex>
-    </FormControl>
+import { Box, FormControl, FormLabel, Flex, Button, Textarea, Radio, RadioGroup, Stack, Input } from '@chakra-ui/react';
+interface Question {
+  id:number,
+  question:string,
+  type:string,
+  options:[],
+  answer:string | number,
+}
+interface QuestionProps {
+  questionData: Question;
+  handleQuestionResponse: (id: number, val: string  | number) => void;
 }
 
-export default  RatingQuestion
+const Question = ({ questionData, handleQuestionResponse }: QuestionProps) => {
+  const { id, question, type, options,answer } = questionData;
+  const renderQuestionType = () => {
+    switch (type) {
+      case 'rating':
+        return (
+          <FormControl >
+          <FormLabel>{question}</FormLabel>
+          <Flex>
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <Box key={rating} as="span" mr="1">
+                <Button
+                  variant={rating === answer ? "solid" : "outline"}
+                  colorScheme={rating == answer ? "teal" : "gray"}
+                  onClick={() => handleQuestionResponse(id, rating)}
+                >
+                  {rating}
+                </Button>
+              </Box>
+            ))}
+          </Flex>
+        </FormControl>
+        );
+      case 'multiple_choice':
+        return (
+          <FormControl >
+            <FormLabel>{question}</FormLabel>
+            <RadioGroup value={answer} onChange={(e) => {
+              handleQuestionResponse(id, e)
+            }}>
+              <Stack direction="column">
+                {options.map((option: string, index: number) => (
+                  <Radio key={index} value={option}>
+                    {option}
+                  </Radio>
+                ))}
+              </Stack>
+            </RadioGroup>
+          </FormControl>
+        );
+      case 'short_answer':
+        return (
+          <FormControl >
+            <FormLabel>{question}</FormLabel>
+            <Input type="text" onChange={(e) => handleQuestionResponse(id, e.target.value)} />
+          </FormControl>
+        );
+      case 'long_answer':
+        return (
+          <FormControl >
+            <FormLabel>{question}</FormLabel>
+            <Textarea  value={answer} onChange={(e) => handleQuestionResponse(id, e.target.value)} />
+          </FormControl>
+        );
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <Box>
+      {renderQuestionType()}
+    </Box>
+  );
+};
+
+export default Question;
