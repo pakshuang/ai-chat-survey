6a283eaabca727accd7cc1d11b409027d9914fa3
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-25 23:39:53 +0100

    rename to windows-dev.bat

--- windows_run.bat
+++ windows_run.bat
@@ -3,30 +3,28 @@ rem You should already be here in ai-chat-survey
 setlocal
 git pull origin main
 
-set "envFile=.env"
-if not exist "%envFile%" (
-    echo Setting up variables.
-    cp sample.env .env
-    :ask
-    set /p "response=Do you wish to set an OpenAI API key (Strongly recommended)? (Y/N): "
-    if /i "%response%" == "N" goto docker
-    if /i "%response%" == "Y" goto loop
-    echo Invalid response.
-    goto ask
 
-    :loop
-    set /p "openaiapi=What is your OpenAI API key? "
-    :asktwo
-    set /p "response=Do you wish to confirm? (Y/N): "
-    if /i "%response%" == "N" goto loop
-    if /i "%response%" == "Y" goto save
-    echo Invalid response
-    goto asktwo
-    :save
+echo Setting up variables.
+cp sample.env .env
+:ask
+set /p "response=Do you wish to set an OpenAI API key (Strongly recommended)? (Y/N): "
+if /i "%response%" == "N" goto docker
+if /i "%response%" == "Y" goto loop
+echo Invalid response.
+goto ask
 
-    echo OPENAI_API_KEY=%openaiapi% >> .env
-)
+:loop
+set /p "openaiapi=What is your OpenAI API key? "
+:asktwo
+set /p "response=Do you wish to confirm? (Y/N): "
+if /i "%response%" == "N" goto loop
+if /i "%response%" == "Y" goto save
+echo Invalid response
+goto asktwo
+:save
 
+echo OPENAI_API_KEY=%openaiapi% >> .env
+endlocal
 :docker
 docker-compose up --build
 echo Press any key to continue.
