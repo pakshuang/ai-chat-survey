430163fd94372a10176747afcdee0ab661b73680
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-25 23:16:06 +0100

    edit batch file

--- windows_run.bat
+++ windows_run.bat
@@ -7,26 +7,25 @@ set "envFile=.env"
 if not exist "%envFile%" (
     echo Setting up variables.
     cp sample.env .env
+    :ask
+    set /p "response=Do you wish to set an OpenAI API key (Strongly recommended)? (Y/N): "
+    if /i "%response%" == "N" goto docker
+    if /i "%response%" == "Y" goto loop
+    echo Invalid response.
+    goto ask
+
+    :loop
+    set /p "openaiapi=What is your OpenAI API key? "
+    :asktwo
+    set /p "response=Do you wish to confirm? (Y/N): "
+    if /i "%response%" == "N" goto loop
+    if /i "%response%" == "Y" goto save
+    echo Invalid response
+    goto asktwo
+    :save
+
+    echo OPENAI_API_KEY=%openaiapi% >> .env
 )
-
-:ask
-set /p "response=Do you wish to set an OpenAI API key (Strongly recommended)? (Y/N): "
-if /i "%response%" == "N" goto docker
-if /i "%response%" == "Y" goto loop
-echo Invalid response.
-goto ask
-
-:loop
-set /p "openaiapi=What is your OpenAI API key? "
-:asktwo
-set /p "response=Do you wish to confirm? (Y/N): "
-if /i "%response%" == "N" goto loop
-if /i "%response%" == "Y" goto save
-echo Invalid response
-goto asktwo
-:save
-
-echo OPENAI_API_KEY=%openaiapi% >> .env
 
 :docker
 docker-compose up --build
