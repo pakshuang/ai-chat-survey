87a059ac58b7e194f0546ab8986ba04d4d3ee3c9
LAPTOP-EG15LSJU\hungy <e0726431@u.nus.edu>
2024-03-26 21:48:18 +0100

    resolve syntax errors

--- backend/src/database_operations.py
+++ backend/src/database_operations.py
@@ -74,12 +74,9 @@ def summarise(chat_context: str) -> str:
         llm = GPT()
         output = llm.run([
             {"role": "system", "content": "You are an assistant who summarises text."},
-            {"role": "user", "content": f"The following text will supply contextual knowledge needed for a survey. \
-<<<<<<< HEAD
-             Summarise it in less than 5 sentences: {chat_context}"}
-=======
-             Summarise it in less than 5 sentences, paying attention to what the survey is about and/or the product: {chat_context}"}
->>>>>>> 7edf911fde48bb6e0faad8311d1c5e614ebf8622
+            {"role": "user", "content": f"""The following text will supply contextual knowledge needed for a survey. 
+             Summarise it in less than 5 sentences, paying attention to what the survey is about and/or the product: 
+             {chat_context}"""}
         ])
         output = output[:MAX_LEN]
         return output
