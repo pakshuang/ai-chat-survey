c7489221d9efd066e553ad7365bedb7a4ed4c41b
xxfcbjhxx <aujunhui88@gmail.com>
2024-03-27 18:40:21 +0800

    Fix formatting

--- backend/tests/test_database.py
+++ backend/tests/test_database.py
@@ -12,12 +12,16 @@ def test_create_admin():
     # Print the response content
     print("Response content:", response.json())
     # assert response.status_code == 201
-    assert response.json()["message"] == "Admin test_admin created successfully" or \
-           response.json()["message"] == "Admin already exists"
+    assert (
+        response.json()["message"] == "Admin test_admin created successfully"
+        or response.json()["message"] == "Admin already exists"
+    )
 
 
 def test_login_admin():
-    url = "http://localhost:{}/api/v1/admins/login".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    url = "http://localhost:{}/api/v1/admins/login".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
     data = {"username": "test_admin", "password": "test_password"}
     response = requests.post(url, json=data)
     print(response)
@@ -30,13 +34,16 @@ def test_login_admin():
 
 def test_create_survey():
     create_survey_url = "http://localhost:{}/api/v1/surveys".format(
-        "5000")  # Assuming BACKEND_CONTAINER_PORT is defined
-    login_url = "http://localhost:{}/api/v1/admins/login".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
+    login_url = "http://localhost:{}/api/v1/admins/login".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
 
     # Login first
     login_data = {"username": "test_admin", "password": "test_password"}
     login_response = requests.post(login_url, json=login_data)
-    jwt_token = login_response.json().get('jwt')
+    jwt_token = login_response.json().get("jwt")
 
     # assert jwt_token, "JWT token not obtained"
 
@@ -47,7 +54,7 @@ def test_create_survey():
             "name": "Test Survey",
             "description": "This is a test survey",
             "created_by": "test_admin",
-            "created_at": "2024-03-22 15:24:10"
+            "created_at": "2024-03-22 15:24:10",
         },
         "title": "Test Title",
         "subtitle": "Test Subtitle",
@@ -56,40 +63,44 @@ def test_create_survey():
                 "id": 1,
                 "type": "multiple_choice",
                 "question": "Which performance did you enjoy the most?",
-                "options": ["Clowns", "Acrobats", "Jugglers", "Magicians", "Choon"]
+                "options": ["Clowns", "Acrobats", "Jugglers", "Magicians", "Choon"],
             },
             {
                 "id": 2,
                 "type": "short_answer",
                 "question": "What did you like about the performance?",
-                "options": []  # Empty list for open-ended question
+                "options": [],  # Empty list for open-ended question
             },
             {
                 "id": 3,
                 "type": "long_answer",
                 "question": "Do you have any feedback about the venue?",
-                "options": []  # Empty list for open-ended question
-            }
+                "options": [],  # Empty list for open-ended question
+            },
         ],
-        "chat_context": "Full Stack Entertainment is an events company that organises performances such as concerts."
+        "chat_context": "Full Stack Entertainment is an events company that organises performances such as concerts.",
     }
 
-    headers = {
-        "Authorization": "Bearer " + jwt_token
-    }
+    headers = {"Authorization": "Bearer " + jwt_token}
 
     # Create a survey
-    create_survey_response = requests.post(create_survey_url, json=survey_data, headers=headers)
+    create_survey_response = requests.post(
+        create_survey_url, json=survey_data, headers=headers
+    )
 
     # Assertions
     # assert create_survey_response.status_code == 201, "Survey creation failed"
     # assert "survey_id" in create_survey_response.json(), "Survey ID not returned"
-    print("Survey created successfully. Survey ID:", create_survey_response.json()["survey_id"])
+    print(
+        "Survey created successfully. Survey ID:",
+        create_survey_response.json()["survey_id"],
+    )
 
 
 def test_get_surveys():
     url = ("http://localhost:{}/api/v1/surveys").format(
-        "5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
 
     print(url)
     # Send GET request to get surveys
@@ -103,7 +114,9 @@ def test_get_surveys():
 
 
 def test_get_survey():
-    url = "http://localhost:{}/api/v1/surveys/3".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    url = "http://localhost:{}/api/v1/surveys/3".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
     print(url)
 
     # Send GET request to get surveys
@@ -118,36 +131,36 @@ def test_get_survey():
 
 def test_submit_response():
     # Define the URL for submitting responses
-    url = "http://localhost:{}/api/v1/responses".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    url = "http://localhost:{}/api/v1/responses".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
 
     # Sample response data with correct structure and values
     response_data = {
-        "metadata": {
-            "survey_id": 3
-        },
+        "metadata": {"survey_id": 3},
         "answers": [
             {
                 "question_id": 1,
                 "type": "multiple_choice",
                 "question": "Which performance did you enjoy the most?",
                 "options": ["Clowns", "Acrobats", "Jugglers", "Magicians", "Choon"],
-                "answer": "Clowns"
+                "answer": "Clowns",
             },
             {
                 "question_id": 2,
                 "type": "short_answer",
                 "question": "What did you like about the performance?",
                 "options": [],  # Empty list provided for short answer question
-                "answer": "I enjoyed the acrobatic stunts."
+                "answer": "I enjoyed the acrobatic stunts.",
             },
             {
                 "question_id": 3,
                 "type": "long_answer",
                 "question": "Do you have any feedback about the venue?",
                 "options": [],  # Empty list provided for long answer question
-                "answer": "The venue was spacious and well-maintained."
-            }
-        ]
+                "answer": "The venue was spacious and well-maintained.",
+            },
+        ],
     }
 
     # Send POST request to submit response
@@ -159,20 +172,22 @@ def test_submit_response():
 
 
 def test_get_responses():
-    url = "http://localhost:{}/api/v1/responses".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    url = "http://localhost:{}/api/v1/responses".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
     survey_id = 3  # Assuming the survey ID for testing
-    login_url = "http://localhost:{}/api/v1/admins/login".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    login_url = "http://localhost:{}/api/v1/admins/login".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
     # Login first
     data = {"username": "test_admin", "password": "test_password"}
     response = requests.post(login_url, json=data)
 
     # Get jwt token
-    jwt_token = response.json()['jwt']
+    jwt_token = response.json()["jwt"]
     print(jwt_token)
 
-    headers = {
-        "Authorization": "Bearer " + jwt_token  # Include your admin token here
-    }
+    headers = {"Authorization": "Bearer " + jwt_token}  # Include your admin token here
 
     # Send GET request to get responses for the survey
     response = requests.get(url, params={"survey": survey_id}, headers=headers)
@@ -183,18 +198,18 @@ def test_get_responses():
 def test_get_response():
     url = "http://localhost:{}/api/v1/responses/{}?survey={}".format("5000", 1, 3)
 
-    login_url = "http://localhost:{}/api/v1/admins/login".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    login_url = "http://localhost:{}/api/v1/admins/login".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
     # Login first
     data = {"username": "test_admin", "password": "test_password"}
     response = requests.post(login_url, json=data)
 
     # Get jwt token
-    jwt_token = response.json()['jwt']
+    jwt_token = response.json()["jwt"]
     print(jwt_token)
 
-    headers = {
-        "Authorization": "Bearer " + jwt_token  # Include your admin token here
-    }
+    headers = {"Authorization": "Bearer " + jwt_token}  # Include your admin token here
 
     # Send GET request to get responses for the survey
     response = requests.get(url, headers=headers)
@@ -206,9 +221,7 @@ def test_send_chat_message():
     url = "http://localhost:{}/api/v1/responses/{}/chat?survey={}".format("5000", 1, 3)
 
     # Prepare response data
-    response_data = {
-        "content": ""
-    }
+    response_data = {"content": ""}
 
     # Send POST request to send chat message
     response = requests.post(url, json=response_data)
@@ -220,18 +233,18 @@ def test_send_chat_message():
 
 
 def test_delete_survey():
-    login_url = "http://localhost:{}/api/v1/admins/login".format("5000")  # Assuming BACKEND_CONTAINER_PORT is defined
+    login_url = "http://localhost:{}/api/v1/admins/login".format(
+        "5000"
+    )  # Assuming BACKEND_CONTAINER_PORT is defined
     # Login first
     data = {"username": "test_admin", "password": "test_password"}
     response = requests.post(login_url, json=data)
 
     # Get jwt token
-    jwt_token = response.json()['jwt']
+    jwt_token = response.json()["jwt"]
     print(jwt_token)
 
-    headers = {
-        "Authorization": "Bearer " + jwt_token  # Include your admin token here
-    }
+    headers = {"Authorization": "Bearer " + jwt_token}  # Include your admin token here
 
     # URL for deleting a survey
     delete_survey_url = "http://localhost:{}/api/v1/surveys/3".format("5000")
