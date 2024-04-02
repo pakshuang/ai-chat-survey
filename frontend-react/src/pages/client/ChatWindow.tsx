8f753cf6c3072b183e7395b8d3e4a5f58bf5f962
Nixon <nixonwidjaja23@gmail.com>
2024-04-03 00:18:40 +0800

    fix: key

--- frontend-react/src/pages/client/ChatWindow.tsx
+++ frontend-react/src/pages/client/ChatWindow.tsx
@@ -1,9 +1,9 @@
-import { Box, SkeletonCircle, Button, Flex } from "@chakra-ui/react";
-import { useRef, useEffect, useState } from "react";
-import ChatMessage from "./ChatMessage";
-import TypingEffect from "./TypingEffect";
-import QuestionInput from "./QuestionInput";
-import { ChatWindowProps, surveyMessage } from "./constants";
+import { Box, SkeletonCircle, Button, Flex } from "@chakra-ui/react"
+import { useRef, useEffect, useState } from "react"
+import ChatMessage from "./ChatMessage"
+import TypingEffect from "./TypingEffect"
+import QuestionInput from "./QuestionInput"
+import { ChatWindowProps, surveyMessage } from "./constants"
 
 function ChatWindow({
   messages,
@@ -12,21 +12,21 @@ function ChatWindow({
   surveyState,
   handleSubmit,
 }: ChatWindowProps) {
-  const messagesEndRef = useRef<HTMLDivElement>(null);
-  const [botResponded, setBotResponded] = useState(false);
+  const messagesEndRef = useRef<HTMLDivElement>(null)
+  const [botResponded, setBotResponded] = useState(false)
   const scrollToBottom = () => {
-    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  };
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" })
+  }
 
   useEffect(() => {
     if (messages.slice(-1)[0]?.sender === "bot") {
-      setBotResponded(true);
+      setBotResponded(true)
     }
     if (messages.slice(-1)[0]?.sender === "user") {
-      setBotResponded(false);
+      setBotResponded(false)
     }
-    scrollToBottom();
-  }, [messages]);
+    scrollToBottom()
+  }, [messages])
   return (
     <Box
       overflowY="auto"
@@ -57,27 +57,33 @@ function ChatWindow({
           if (item.message === surveyMessage) {
             if (surveyState.submitted) {
               return (
-                <ChatMessage sender={item.sender}>{item.message}</ChatMessage>
-              );
+                <ChatMessage key={index} sender={item.sender}>
+                  {item.message}
+                </ChatMessage>
+              )
             }
             return (
-              <ChatMessage sender={"bot"}>
+              <ChatMessage key={index} sender={"bot"}>
                 <Flex flexDirection="column">
                   <TypingEffect
                     text="Thank you for your responses. Please confirm your answers now, as they can't be changed later. Once confirmed, we'll continue with our discussion."
                     scrollToBottom={scrollToBottom}
                   ></TypingEffect>
                   <Box>
-                    <Button onClick={handleSubmit} colorScheme="green" mt="0.5rem">
+                    <Button
+                      onClick={handleSubmit}
+                      colorScheme="green"
+                      mt="0.5rem"
+                    >
                       Confirm
                     </Button>
                   </Box>
                 </Flex>
               </ChatMessage>
-            );
+            )
           }
           return (
-            <ChatMessage sender="bot">
+            <ChatMessage key={index} sender="bot">
               <TypingEffect
                 text={messages.slice(-1)[0].message}
                 scrollToBottom={scrollToBottom}
@@ -88,10 +94,10 @@ function ChatWindow({
                 submitted={surveyState.submitted}
               ></QuestionInput>
             </ChatMessage>
-          );
+          )
         } else {
           return (
-            <ChatMessage sender={item.sender}>
+            <ChatMessage key={index} sender={item.sender}>
               {item.message}
               <QuestionInput
                 questionData={item.question}
@@ -99,7 +105,7 @@ function ChatWindow({
                 submitted={surveyState.submitted}
               ></QuestionInput>
             </ChatMessage>
-          );
+          )
         }
       })}
       {isBotThinking && (
@@ -109,7 +115,7 @@ function ChatWindow({
       )}
       <div ref={messagesEndRef} />
     </Box>
-  );
+  )
 }
 
-export default ChatWindow;
+export default ChatWindow
