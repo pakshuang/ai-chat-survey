9f7d7426e7596b492234aabb7b0b1ee5f0813801
HY <e0726431@u.nus.edu>
2024-04-07 19:00:55 +0800

    add finetuning script

--- scripts/training/train_script.py
+++ scripts/training/train_script.py
@@ -1,51 +1,47 @@
 import argparse
 from datasets import Dataset, DatasetDict
 import pandas as pd
-from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+import pathlib
+from transformers import AutoModelForCausalLM, AutoTokenizer, Trainer, TrainingArguments, DataCollatorForLanguageModeling
+from transformers import GPTQConfig
 from peft import LoraConfig, get_peft_model, get_peft_model, prepare_model_for_kbit_training, PeftModel
-
+import random
 
 def main():
     # Create argument parser
     parser = argparse.ArgumentParser(description='Training model')
 
 
-    parser.add_argument('--base', dest='base_model', type=str, help='Base model (huggingface name or path) to finetune')
+    parser.add_argument('--base', dest='base_model', type=str, help='Base model (huggingface name or path to GPTQ folder) to finetune')
     parser.add_argument('--dataset-path', dest='dataset', type=str, help='path to dataset. Dataset MUST contain columns `input` and `output` and if needed, `system`')
-    parser.add_argument('--output-path', dest='output', type=str, help='Output path')
+    parser.add_argument('--bits', dest='bits', type=int, help='Number of bits of quantised model')
 
+    parser.add_argument('--output-path', dest='output', type=str, help='Output path', default = str(pathlib.Path(__file__).parent.resolve()) + "/../../backend/models")
     parser.add_argument('--learning-rate', dest='lr', type=float, help='Learning rate', default=2.5e-4)
     parser.add_argument('--steps', dest='steps', type=int, help='Number of steps to train', default=256)
     parser.add_argument('--r', dest='r', type=int, help='rank parameter', default=16)
     parser.add_argument('--lora-dropout', dest='dropout', type=float, help='dropout parameter', default=0.05)
     parser.add_argument('--lora-alpha', dest='alpha', type=int, help='dropout parameter', default=16)
     parser.add_argument('--train-size', dest='train', type=float, help='size of training dataset as a proportion (0-1)', default=0.75)
-    parser.add_argument('--system', dest='has_sys', type=int, help='Does the model support system prompts in their chat template 1 or 0', default=1)
+    parser.add_argument('--has-system-prompt', dest='has_sys', type=int, help='Does the model support system prompts in their chat template 1 or 0', default=1)
     parser.add_argument('--max-len', dest='max_len', type=int, help='Max generation length', default=256)
-
+    parser.add_argument('--seed', dest='seed', type=int, help='Seed used when splitting dataset. The value -1 will be interpreted as an un-set seed', default=-1)
+    parser.add_argument('--group_size', type=int, default=128, help='Specify GPTQ group_size.')
     # Parse the arguments
     args = parser.parse_args()
 
     # Access the argument values
-    bnb_config = BitsAndBytesConfig(
-        load_in_4bit=True,
-        bnb_4bit_quant_type="nf4",
-        bnb_4bit_compute_dtype="float16",
-    )
-    print("Loading model...")
+    
+    gptq_config = GPTQConfig(bits=args.bits, disable_exllama=True)
+
     base = AutoModelForCausalLM.from_pretrained(
         args.base_model,
-        quantization_config=bnb_config,
-        trust_remote_code=True,
+        quantization_config=gptq_config,
         device_map="auto",
     )
     tokenizer = AutoTokenizer.from_pretrained(
         args.base_model, 
-        trust_remote_code=True, 
-        return_token_type_ids=False
     )
-    tokenizer.pad_token = tokenizer.eos_token
-    tokenizer.padding_side = "right"
 
     base.gradient_checkpointing_enable()
     base = prepare_model_for_kbit_training(base)
@@ -59,18 +55,18 @@ def main():
         task_type="CAUSAL_LM",
     )
     model = get_peft_model(base, config)
-
-    # Dataset stuf
+    model.config.use_cache = False
     df = pd.read_csv(args.dataset)
     ds = Dataset.from_pandas(df)
-   
-    ds_splits = split_dataset(ds, args)
+    if args.seed == -1:
+        ds_splits = split_dataset(ds, args)
+    else:
+        ds_splits = split_dataset(ds, args, seed=args.seed)
     
     train = ds_splits["train"].map(lambda row: {'input_ids': format_input_outputs(row, args.has_sys, tokenizer, args.max_len)})
     test = ds_splits["test"].map(lambda row: {'input_ids': format_input_outputs(row, args.has_sys, tokenizer, args.max_len)})
     valid = ds_splits["valid"].map(lambda row: {'input_ids': format_input_outputs(row, args.has_sys, tokenizer, args.max_len)})
 
-    print("Training")
 
     trainer = Trainer(
     model=model,
@@ -95,13 +91,14 @@ def main():
     
     )
     trainer.train()
+    trainer.model.save_pretrained(args.output)
 
  
     # Your script logic here
 
-def split_dataset(ds: Dataset, args: argparse.ArgumentParser):
-    ds_train_devtest = ds['train'].train_test_split(test_size=1-args.train, seed=42)
-    ds_devtest = ds_train_devtest['test'].train_test_split(test_size=0.5, seed=42)
+def split_dataset(ds: Dataset, args: argparse.ArgumentParser, seed: int = 1):
+    ds_train_devtest = ds.train_test_split(test_size=1-args.train, seed=seed)
+    ds_devtest = ds_train_devtest['test'].train_test_split(test_size=0.5, seed=seed)
     ds_splits = DatasetDict({
         'train': ds_train_devtest['train'],
         'valid': ds_devtest['train'],
