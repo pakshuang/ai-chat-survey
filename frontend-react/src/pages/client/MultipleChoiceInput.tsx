5da071f2065b595e430eabd7560b7264ed08a0df
Vincent <vincent.aurellio@u.nus.edu>
2024-04-08 10:25:52 +0800

    add functionality to multiple choice input

--- frontend-react/src/pages/client/MultipleChoiceInput.tsx
+++ frontend-react/src/pages/client/MultipleChoiceInput.tsx
@@ -1,51 +1,67 @@
 import { Radio, RadioGroup, Button, Flex } from "@chakra-ui/react";
+import { useState } from "react";
+import { MultipleChoiceInputProps } from "./constants";
 
-function MultipleChoiceInput() {
-  const options = [
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mollis.",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mollis..",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mollis...",
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus mollis....",
-  ];
+function MultipleChoiceInput({
+  questionID,
+  options,
+  handleQuestionResponse,
+}: MultipleChoiceInputProps) {
+  const [answer, setAnswer] = useState<string>("");
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const isAnswered = answer !== "";
+
+  function handleSubmit(e: React.MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setIsSubmitting(true);
+    handleQuestionResponse(questionID, answer);
+  }
+
   return (
-    <Flex flexDirection="row" p="0.5rem" w="60rem" mx="auto">
-      <RadioGroup
-        size="lg"
-        p="1rem"
-        display="flex"
-        backgroundColor="white"
-        alignItems="left"
-        flex="1"
-        flexDirection="column"
-        gap="1.5"
-        justifyContent="space-evenly"
-        borderRadius="md"
-        borderRightRadius="0"
-        border="1px"
-        borderColor="gray.500"
-      >
-        {options.map((option: string, index: number) => (
-          <Radio
-            borderColor="black"
-            colorScheme="blue"
-            key={index}
-            value={option}
-          >
-            {option}
-          </Radio>
-        ))}
-      </RadioGroup>
-      <Button
-        size="lg"
-        type="submit"
-        colorScheme="blue"
-        borderColor="gray.500"
-        borderLeftRadius="0"
-        height="auto"
-      >
-        Send
-      </Button>
-    </Flex>
+    <form>
+      <Flex flexDirection="row" p="0.5rem" w="60rem" mx="auto">
+        <RadioGroup
+          size="lg"
+          p="1rem"
+          display="flex"
+          backgroundColor="white"
+          alignItems="left"
+          flex="1"
+          flexDirection="column"
+          gap="1.5"
+          justifyContent="space-evenly"
+          borderRadius="md"
+          borderRightRadius="0"
+          border="1px"
+          borderColor="gray.500"
+          onChange={(e) => setAnswer(e)}
+        >
+          {options.map((option: string, index: number) => (
+            <Radio
+              borderColor="black"
+              colorScheme="blue"
+              key={index}
+              value={option}
+            >
+              {option}
+            </Radio>
+          ))}
+        </RadioGroup>
+        <Button
+          size="lg"
+          type="submit"
+          colorScheme="blue"
+          borderColor="gray.500"
+          borderLeftRadius="0"
+          height="auto"
+          onClick={handleSubmit}
+          isDisabled={!isAnswered}
+          isLoading={isSubmitting}
+        >
+          Send
+        </Button>
+      </Flex>
+    </form>
   );
 }
 
