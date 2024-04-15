4813db49037b8679cc380c9c85d16c695b929089
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2024-04-15 17:01:11 +0800

    Update reverse-proxy/generate_nginx_config.sh and sample.env files

--- reverse-proxy/generate_nginx_config.sh
+++ reverse-proxy/generate_nginx_config.sh
@@ -2,6 +2,14 @@
 ENV=${NGINX_ENV:-development}
 
 if [ "$ENV" = "production" ]; then
+    apt-get update
+    apt-get install -y software-properties-common
+    add-apt-repository ppa:certbot/certbot
+    apt-get update
+    apt-get install -y nginx certbot python3-certbot-nginx
+    certbot --nginx -d ${DOMAIN} \
+    --email ${CERT_EMAIL} --agree-tos --no-eff-email \
+    --keep-until-expiring --non-interactive
     SSL="ssl"
     SSL_CERTIFICATE="ssl_certificate /etc/letsencrypt/live/${DOMAIN}/fullchain.pem;"
     SSL_CERTIFICATE_KEY="ssl_certificate_key /etc/letsencrypt/live/${DOMAIN}/privkey.pem;"
