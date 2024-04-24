1c10acd97a84a6eb5ac805bbeaead267e8ec2cf5
Lee Pak Shuang <81917538+pakshuang@users.noreply.github.com>
2024-04-24 22:39:39 +0800

    Update setup.md with SSL deployment instructions

--- scripts/init_ssl.sh
+++ scripts/init_ssl.sh
@@ -9,7 +9,7 @@ domains=($1)
 rsa_key_size=4096
 data_path="./certbot"
 email="$2" # Adding a valid address is strongly recommended
-staging=$3 # Set to 1 if you're testing your setup to avoid hitting request limits
+staging="$3" # Set to 1 if you're testing your setup to avoid hitting request limits
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
